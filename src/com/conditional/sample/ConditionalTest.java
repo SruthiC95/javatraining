1f3799e2f0d6d3dd057429c77cf7fdaa9533e816
sruthi <sruthi.c1995@gmail.com>
2022-02-21 23:43:53 +0100

    first project

---
+++ src/com/conditional/sample/ConditionalTest.java
@@ -0,0 +1,24 @@
+package com.conditional.sample;
+
+import java.lang.String;
+public class ConditionalTest {
+	String test=null;
+	int a ;
+	float b;
+	double db;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ConditionalTest ct=new ConditionalTest();
+		ct.test();
+	
+		
+	}
+	
+	public void test() {
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(db);
+	}
+}
---
+++ src/com/loop/sample/Test.java
@@ -0,0 +1,12 @@
+package com.loop.sample;
+
+import com.conditional.sample.ConditionalTest;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ConditionalTest obj=new ConditionalTest();
+	}
+
+}
